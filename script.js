10a318cecc7b0133a2e9dd689b204973027ab679
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-04-29 14:38:53 +0100

    random number and display dice number added

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// SELECTING ELEMENTS
+const score0El = document.getElementById("score--0"); // OR document.querySelector('#score--0') // id includes hashtag
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//  ADD BUTTONS VARIABLE
+
+// Starting Condition
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+// Roll dice functionality
+
+// 1. add event listener on btn and generate random number 1-6
+btnRoll.addEventListener("click", function () {
+  const dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice roll
+  diceEl.classList.remove("hidden");
+  diceEl.src = `./public/dice-${dice}.png`;
+
+  // 3. check if 1 is rolled
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
