843317e11d6bfa5d63288ac78306e97380c0f581
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-04-29 15:21:21 +0100

    switch player functionality added and tested

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 // SELECTING ELEMENTS
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0El = document.getElementById("score--0"); // OR document.querySelector('#score--0') // id includes hashtag
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -19,7 +22,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 // Roll dice functionality
 
 // 1. add event listener on btn and generate random number 1-6
@@ -34,8 +39,15 @@ btnRoll.addEventListener("click", function () {
   // 3. check if 1 is rolled
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
