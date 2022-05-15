c5822259ca3758678c752c35e5312bbf94ece302
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-15 10:11:32 +0100

    modal feature, how to play added

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 "use strict";
 
+// ================= MODAL FUNCTIONALITY ================= //
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// open modal with overlay effect
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
+}
+
+// close modal when clicked
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+
+// close modal when clicked
+overlay.addEventListener("click", closeModal);
+
+// close modal with escape key
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// ================= ========================= ================= //
+
 // SELECTING ELEMENTS
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
